23ef4e5ee112e7dd02a845aeb3ef97c3469d5d85
chris6661 <74381468+chris6661@users.noreply.github.com>
2021-02-10 14:45:42 -0600

    refactored player object js file

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -15,3 +15,22 @@ test('creates a player object', () => {
       expect.arrayContaining([expect.any(Object)])
     );
   });
+//typically only run one test at a time; can run more when methods aer simple data returns
+  test("gets player's stats as an object", () => {
+    const player = new Player('Dave');
+  
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+  });
+
+  test('gets inventory from player or returns false', () => {
+    const player = new Player('Dave');
+  
+    expect(player.getInventory()).toEqual(expect.any(Array));
+  
+    player.inventory = [];
+  
+    expect(player.getInventory()).toEqual(false);
+  });
